aa2c5ddd9ce8f0dd259d18774945736a26017682
sher0501 <asoggamer@gmail.com>
2022-04-20 16:43:21 +0500

    imtihon

---
+++ Js/index.js
@@ -0,0 +1,10 @@
+// let arr = ["I", "study", "JavaScript", "right", "now"];
+// arr.splice(2, 1);
+// console.log(arr);
+let a = "Salom dunyo";
+function reverse(a) {
+  a = str.split("").reverse().join("");
+  return a;
+}
+let b = reverse(a);
+console.log(b);
